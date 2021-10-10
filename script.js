7e2bbc3c2d3b470e8f6b3e68dd3492075ad1e20c
Bae Seo Hyeon <bae4614@gmail.com>
2021-10-10 15:15:56 +0900

    feat: initialize

---
+++ script.js
@@ -0,0 +1,6 @@
+function goIndex() { location.replace('index.html'); } 
+function goIntro() { location.replace('intro.html'); } 
+function goHospital() { location.replace('hospital.html'); } 
+function goMedicine() { location.replace('medicine.html'); } 
+function goPharmacy() { location.replace('pharmacy.html'); } 
+function goStore() { location.replace('store.html'); } 
